74187e725ad5eff135c65f50698388bdd323a78d
Schizohatter <soanvig@gmail.com>
2017-12-18 22:55:24 +0100

    PostCSS now resolves Webpack aliases

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,9 +1,19 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
+const createResolver = require('postcss-import-webpack-resolver');
+const webpackConfig = require('./build/webpack.base.conf');
 
 module.exports = {
   'plugins': {
     // to edit target browsers: use "browserslist" field in package.json
-    'postcss-import': {},
+    'postcss-import': {
+      resolve: createResolver({
+        // use aliases defined in config 
+        alias: webpackConfig.resolve.alias,
+   
+        // include where to look for modules 
+        modules: ['src', 'node_modules']
+      })
+    },
     'postcss-each': {},
     'postcss-for': {},
     'postcss-selector-not': {},
